004336bddd121d43cc1802ff38cef3a2f3114894
kotnid <tkt0506tkt@gmail.com>
2023-07-26 10:34:47 +0800

    e-leaflet : Add animation on swipe
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var dropdown = document.getElementsByClassName("dropdown")[0];
+var dropdown = document.getElementsByClassName("dropdown")[0];  
+var lastScrollTime = 0;
+var lastScrollY = 0;
+var scrollSpeed = 0;
+var arrow = document.getElementsByClassName("arrow")[0];  
 
 window.addEventListener('scroll', function() {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var viewportHeight = window.innerHeight;
     var pages = Math.floor(scrollTop/viewportHeight+0.95);
 
+    var swipe = document.querySelector('.swipe');
     var swipeText = document.querySelector('.swipe-text');
     var swipeTextColor = window.getComputedStyle(swipeText).getPropertyValue('color');
-    console.log(swipeTextColor);
 
     if(pages%2==0){
         if(swipeTextColor == "rgb(153, 153, 153)") {
@@ -18,6 +22,15 @@ window.addEventListener('scroll', function() {
             swipeText.style.color = "#999999";
         }
     }
+
+    
+    if(pages != 4 && swipe.classList.contains('hidden')){
+        swipe.classList.remove('hidden');
+    }
+
+    if(pages == 4 && !(swipe.classList.contains('hidden'))){
+        swipe.classList.add('hidden');
+    }
   });
 
 for(var i=0 ; i<dropdown.childNodes.length; i++){
@@ -27,13 +40,13 @@ for(var i=0 ; i<dropdown.childNodes.length; i++){
     box.addEventListener("click", function(){
         var dropdownContent = this.querySelector('.box-content');
         collapseAll(this);
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-            this.style.height = "5.25vh";
-        } else {
-            dropdownContent.style.display = "block";
-            this.style.height = "10.5vh";
-        }
+        if (dropdownContent.classList.contains('hidden')) {
+            dropdownContent.classList.remove('hidden');
+            this.style.height = '10.5vh';
+          } else {
+            dropdownContent.classList.add('hidden');
+            this.style.height = '5.25vh';
+          }
     })
 }
 
@@ -44,10 +57,20 @@ function collapseAll(crt){
         if(box == crt)continue;
 
         var dropdownContent = box.querySelector('.box-content');
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-            box.style.height = "5.25vh";
-        }
+        dropdownContent.classList.add('hidden');
+        box.style.height = '5.25vh';
     }
 }
+
+window.addEventListener('wheel', function(event) {
+    var now = Date.now();
+    var timeDiff = now - lastScrollTime;
+    var distance = event.pageY - lastScrollY;
+    var speed = Math.abs(distance / timeDiff);
+    console.log(speed);
+    // arrow.style.transform = "translateY(" + speed/5 +"vh)";
+    scrollSpeed = speed;
+    lastScrollTime = now;
+    lastScrollY = event.pageY;
+  });
 
